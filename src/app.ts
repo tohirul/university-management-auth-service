4418ee6463fc9f0fa733a368be1e2a1d76998caa
tohirul <tohirul.islam164@gmail.com>
2023-07-02 22:33:46 +0600

    [Done] Setup: eslint, prettier, husky, lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express, {Application,Request,Response,NextFunction} from 'express';
-import cors from "cors"
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app:Application = express();
+const app: Application = express()
 app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({ extended:true}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/",(req:Request, res:Response,next:NextFunction) => {
-    res.send("Hi, Server is running")
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hi, Server is running')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
-import config from "./config/index"
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './config/index'
+import app from './app'
 
-async function main(){
-    try{
-        await mongoose.connect(config.database_url as string)
-        console.log("Database connection established")
-        app.listen(config.port,()=>{
-            console.log(`Application listening on ${config.port}`)
-        })
-    }catch(err:any){
-        console.log("Failed to connect to Mongo database",err);
-    }
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connection established')
+    app.listen(config.port, () => {
+      console.log(`Application listening on ${config.port}`)
+    })
+  } catch (err: any) {
+    console.log('Failed to connect to Mongo database', err)
+  }
 }
 main()
 
-
-// 
+//
