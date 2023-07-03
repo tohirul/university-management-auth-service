ef5eecda0bcbd89ad95918f6a9018fc0a264e009
tohirul <tohirul.islam164@gmail.com>
2023-07-03 22:07:14 +0600

    [Done] Setup: userInterface && userModel && User

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+// import { dbConnect } from './dbConnect';
 
-const app: Application = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hi, Server is running')
-})
+  res.send('Hi, Server is running');
+});
 
-export default app
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,35 @@
-import mongoose from 'mongoose'
-import config from './config/index'
-import app from './app'
+import http from 'http';
+import mongoose from 'mongoose';
 
-async function main() {
+import { log } from './app/utility/logger';
+import app from './app';
+import config from './config';
+import { dbConnect } from './dbConnect';
+
+const PORT = config.port;
+let server: http.Server;
+const toggleServer = async (): Promise<void> => {
   try {
-    await mongoose.connect(config.database_url as string)
-    console.log('Database connection established')
-    app.listen(config.port, () => {
-      console.log(`Application listening on ${config.port}`)
-    })
-  } catch (err: any) {
-    console.log('Failed to connect to Mongo database', err)
+    dbConnect();
+    server = app.listen(PORT, () => {
+      log.info(`Server is breathing on ${PORT}`);
+    });
+  } catch (error) {
+    log.error(error);
   }
-}
-main()
+};
+process.on('SIGINT', async () => {
+  try {
+    if (server) {
+      server.close();
+    }
+    await mongoose.disconnect();
+    log.warn('Server and database connection will be closed');
+    process.exit(0);
+  } catch (error) {
+    log.info(error);
+    process.exit(1);
+  }
+});
 
-//
+toggleServer();
