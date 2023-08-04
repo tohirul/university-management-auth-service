a0a6d4d5a80c3cb95a34accf2b0c278fd88e5c6c
tohirul <tohirul.islam164@gmail.com>
2023-08-04 19:32:51 +0600

    [Fix] Vercel Logger Error

--- dist/server.js
+++ dist/server.js
@@ -8,18 +8,17 @@ const process_1 = __importDefault(require("process"));
 const app_1 = __importDefault(require("./app"));
 const config_1 = __importDefault(require("./config"));
 const dbConnect_1 = __importDefault(require("./dbConnect"));
-const log_1 = __importDefault(require("./shared/log"));
 const PORT = config_1.default.port;
 let server;
 const toggleServer = async () => {
     try {
         (0, dbConnect_1.default)();
         server = app_1.default.listen(PORT, () => {
-            log_1.default.checkInfo.info(`Server is breathing on ${PORT}`);
+            console.info(`Server is breathing on ${PORT}`);
         });
     }
     catch (error) {
-        log_1.default.checkError.error(error);
+        console.error(error);
     }
 };
 process_1.default.on('SIGINT', async () => {
@@ -29,29 +28,29 @@ process_1.default.on('SIGTERM', async () => {
     handleServerShutdown('SIGTERM');
 });
 process_1.default.on('unhandledRejection', async (error) => {
-    log_1.default.checkError.error('unhandledRejection', error);
+    console.error('unhandledRejection', error);
     handleServerShutdown('unhandledRejection', error);
 });
 process_1.default.on('uncaughtException', (error) => {
-    log_1.default.checkError.error('Uncaught Exception:', error);
+    console.error('Uncaught Exception:', error);
     handleServerShutdown('uncaughtException', error);
 });
 const handleServerShutdown = async (eventName, error) => {
-    log_1.default.checkInfo.warn(`Server received ${eventName} signal. Server connection will be closed.`);
+    console.warn(`Server received ${eventName} signal. Server connection will be closed.`);
     if (eventName === 'SIGINT')
         await mongoose_1.default.disconnect();
     try {
         if (server) {
             server.close(() => {
                 if (error) {
-                    log_1.default.checkError.error(error);
+                    console.error(error);
                 }
             });
         }
         process_1.default.exit(0);
     }
     catch (error) {
-        log_1.default.checkError.error(error);
+        console.error(error);
         process_1.default.exit(1);
     }
 };
