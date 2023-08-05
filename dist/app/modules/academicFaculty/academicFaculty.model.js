513b81df5c7cf8395b6692917697f157de17a9c2
tohirul <tohirul.islam164@gmail.com>
2023-08-05 20:10:16 +0600

    Dist Commit

--- dist/app/modules/academicFaculty/academicFaculty.model.js
+++ dist/app/modules/academicFaculty/academicFaculty.model.js
@@ -1,31 +1,42 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_status_1 = __importDefault(require("http-status"));
-const mongoose_1 = require("mongoose");
-const ApiError_1 = __importDefault(require("../../../errors/ApiError"));
-const academicFacultySchema = new mongoose_1.Schema({
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const http_status_1 = __importDefault(require('http-status'));
+const mongoose_1 = require('mongoose');
+const ApiError_1 = __importDefault(require('../../../errors/ApiError'));
+const academicFacultySchema = new mongoose_1.Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
-}, {
+  },
+  {
     timestamps: true,
     toJSON: {
-        virtuals: true,
+      virtuals: true,
     },
-});
+  }
+);
 academicFacultySchema.pre('save', async function (next) {
-    const exists = await AcademicFaculty.findOne({
-        title: this.title,
-    });
-    if (exists) {
-        throw new ApiError_1.default(http_status_1.default.CONFLICT, 'Faculty already exists in the database, no need to create a new one');
-    }
-    next();
+  const exists = await AcademicFaculty.findOne({
+    title: this.title,
+  });
+  if (exists) {
+    throw new ApiError_1.default(
+      http_status_1.default.CONFLICT,
+      'Faculty already exists in the database, no need to create a new one'
+    );
+  }
+  next();
 });
-const AcademicFaculty = (0, mongoose_1.model)('AcademicFaculty', academicFacultySchema);
+const AcademicFaculty = (0, mongoose_1.model)(
+  'AcademicFaculty',
+  academicFacultySchema
+);
 exports.default = AcademicFaculty;
--- dist/app/modules/academicSemester/academicSemester.route.js
+++ dist/app/modules/academicSemester/academicSemester.route.js
@@ -1,15 +1,37 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const validateRequest_1 = __importDefault(require("../../middlewares/validateRequest"));
-const academicSemester_controller_1 = __importDefault(require("./academicSemester.controller"));
-const academicSemester_validation_1 = __importDefault(require("./academicSemester.validation"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = __importDefault(require('express'));
+const validateRequest_1 = __importDefault(
+  require('../../middlewares/validateRequest')
+);
+const academicSemester_controller_1 = __importDefault(
+  require('./academicSemester.controller')
+);
+const academicSemester_validation_1 = __importDefault(
+  require('./academicSemester.validation')
+);
 const router = express_1.default.Router();
-router.post('/create_semester', (0, validateRequest_1.default)(academicSemester_validation_1.default.createAcademicSemesterZodValidationSchema), academicSemester_controller_1.default.createSemester);
-router.patch('/:id', (0, validateRequest_1.default)(academicSemester_validation_1.default.updateAcademicSemesterZodValidationSchema), academicSemester_controller_1.default.updateSemester);
+router.post(
+  '/create_semester',
+  (0, validateRequest_1.default)(
+    academicSemester_validation_1.default
+      .createAcademicSemesterZodValidationSchema
+  ),
+  academicSemester_controller_1.default.createSemester
+);
+router.patch(
+  '/:id',
+  (0, validateRequest_1.default)(
+    academicSemester_validation_1.default
+      .updateAcademicSemesterZodValidationSchema
+  ),
+  academicSemester_controller_1.default.updateSemester
+);
 router.get('/', academicSemester_controller_1.default.getAllSemesters);
 router.get('/:id', academicSemester_controller_1.default.getSingleSemester);
 router.delete('/:id', academicSemester_controller_1.default.deleteSemester);
