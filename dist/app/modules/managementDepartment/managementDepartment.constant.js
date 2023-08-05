a95305fe9573d98dbb69c3126bee41404363a42e
tohirul <tohirul.islam164@gmail.com>
2023-08-05 19:58:27 +0600

    Dist Update

--- dist/app/modules/managementDepartment/managementDepartment.constant.js
+++ dist/app/modules/managementDepartment/managementDepartment.constant.js
@@ -1,5 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ManagementDepartmentSearchableFields = exports.ManagementDepartmentFilterableFields = void 0;
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.ManagementDepartmentSearchableFields =
+  exports.ManagementDepartmentFilterableFields = void 0;
 exports.ManagementDepartmentFilterableFields = ['searchTerm', 'title'];
 exports.ManagementDepartmentSearchableFields = ['title'];
