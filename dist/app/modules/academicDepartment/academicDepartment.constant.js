76721867ebbb43c140430cdb81c3db30db92eba1
tohirul <tohirul.islam164@gmail.com>
2023-08-06 15:04:26 +0600

    [ Update ] app.js

--- dist/app/modules/academicDepartment/academicDepartment.constant.js
+++ dist/app/modules/academicDepartment/academicDepartment.constant.js
@@ -1,5 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AcademicDepartmentSearchableFields = exports.AcademicDepartmentFilterableFields = void 0;
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.AcademicDepartmentSearchableFields =
+  exports.AcademicDepartmentFilterableFields = void 0;
 exports.AcademicDepartmentFilterableFields = ['searchTerm', 'title'];
 exports.AcademicDepartmentSearchableFields = ['title'];
--- dist/dbConnect.js
+++ dist/dbConnect.js
@@ -1,25 +1,26 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 /* eslint-disable no-console */
-const mongoose_1 = __importDefault(require("mongoose"));
-const process_1 = __importDefault(require("process"));
-const index_1 = __importDefault(require("./config/index"));
+const mongoose_1 = __importDefault(require('mongoose'));
+const process_1 = __importDefault(require('process'));
+const index_1 = __importDefault(require('./config/index'));
 const URI = index_1.default.database_url;
 const dbConnect = async () => {
-    try {
-        if (!URI) {
-            console.log('No URI found in the configuration');
-            process_1.default.exit(1);
-        }
-        await mongoose_1.default.connect(URI).then(() => {
-            console.log('Database Connection Established');
-        });
+  try {
+    if (!URI) {
+      console.log('No URI found in the configuration');
+      process_1.default.exit(1);
     }
-    catch (error) {
-        console.log(error.message);
-    }
+    await mongoose_1.default.connect(URI).then(() => {
+      console.log('Database Connection Established');
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 exports.default = dbConnect;
